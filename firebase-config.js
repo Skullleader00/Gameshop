7aba3a3c00d1080fd1b56395a9484453f427e931
Skullleader00 <81022844+Skullleader00@users.noreply.github.com>
2026-04-25 21:41:39 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,7 @@
 // ============================================================
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
+export const FIREBASE_CONFIG = {
   apiKey: "AIzaSyBLZ3U2_wnybg1EKV6hig_YD-j2q3UFsvQ",
   authDomain: "gameshop-d1362.firebaseapp.com",
   databaseURL: "https://gameshop-d1362-default-rtdb.asia-southeast1.firebasedatabase.app",
