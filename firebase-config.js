d1ce7c5e4bb5cae544ad1b60ac1adfdaa143694d
Skullleader00 <81022844+Skullleader00@users.noreply.github.com>
2026-04-25 20:56:23 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// ============================================================
+//  firebase-config.js
+//  แก้ไขค่าด้านล่างด้วยข้อมูลจาก Firebase Console ของคุณ
+//  Firebase Console → Project Settings → Your Apps → Web App
+// ============================================================
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBLZ3U2_wnybg1EKV6hig_YD-j2q3UFsvQ",
+  authDomain: "gameshop-d1362.firebaseapp.com",
+  databaseURL: "https://gameshop-d1362-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gameshop-d1362",
+  storageBucket: "gameshop-d1362.firebasestorage.app",
+  messagingSenderId: "446170708263",
+  appId: "1:446170708263:web:d30cc27d6b31ef95cb5102",
+  measurementId: "G-CH0HJ0EDKW"
+};
